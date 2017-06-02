7fdbd3c3d5fd749db9098ce9774f00c044b2ccb1
Daniel Roberson <daniel@planethacker.net>
2017-06-02 04:35:00 -0700

    added output_optstrig and output_longopts functions

--- tests/ld_preload-get-vars.c
+++ tests/ld_preload-get-vars.c
@@ -39,16 +39,11 @@ char *getenv(const char *name) {
   return NULL;
 }
 
-
-/* getopt()
- * -- Intercept getopt() and print out flags in "-f @@" format if they take
- *    an argument. Otherwise, Leave a comment.
+/* output_optstring()
+ * -- Parses and displays a getopt optstring in a format usable by fuzz_cli.py
  */
-int getopt(int argc, char * const argv[], const char *optstring) {
+void output_optstring(const char *optstring) {
   int i;
-
-
-  printf("# getopt() optstring = %s\n", optstring);
 
   for (i = 0; optstring[i]; i++) {
     if (optstring[i + 1] == ':') {
@@ -61,33 +56,14 @@ int getopt(int argc, char * const argv[], const char *optstring) {
 
     printf("# End of -%c section\n\n", optstring[i]);
   }
-
-  return -1;
 }
 
 
-/* getopt_long()
- * -- Intercept getopt_long() and print in "--flag @@" format if the flag
- *    accepts an argument. Otherwise, leave a comment.
+/* output_longopts()
+ * -- Parses and displays a longopt structure in a format usable by fuzz_cli.py
  */
-int getopt_long(int argc, char * const argv[], const char *optstring,
-                const struct option *longopts, int *longindex) {
+void output_longopts(const struct option *longopts) {
   int i;
-
-
-  printf("# getopt_long() optstring = %s\n", optstring);
-
-  for (i = 0; optstring[i]; i++) {
-    if (optstring[i + 1] == ':') {
-      printf("# -%c <flag>\n", optstring[i]);
-      printf("-%c @@\n", optstring[i]);
-      i++;
-    } else {
-      printf("# -%c takes no arguments\n", optstring[i]);
-    }
-
-    printf("# End of -%c section\n\n", optstring[i]);
-  }
 
   for (i = 0; longopts[i].name; i++) {
     printf("# Start of --%s section\n", longopts[i].name);
@@ -101,6 +77,31 @@ int getopt_long(int argc, char * const argv[], const char *optstring,
 
     printf("# End of --%s flag section\n\n", longopts[i].name);
   }
+}
+
+
+/* getopt()
+ * -- Intercept getopt() and print out flags in "-f @@" format if they take
+ *    an argument. Otherwise, Leave a comment.
+ */
+int getopt(int argc, char * const argv[], const char *optstring) {
+  printf("# getopt() optstring = %s\n", optstring);
+  output_optstring(optstring);
+
+  return -1;
+}
+
+
+/* getopt_long()
+ * -- Intercept getopt_long() and print in "--flag @@" format if the flag
+ *    accepts an argument. Otherwise, leave a comment.
+ */
+int getopt_long(int argc, char * const argv[], const char *optstring,
+                const struct option *longopts, int *longindex) {
+  printf("# getopt_long() optstring = %s\n", optstring);
+
+  output_optstring(optstring);
+  output_longopts(longopts);
 
   return -1;
 }
@@ -112,35 +113,10 @@ int getopt_long(int argc, char * const argv[], const char *optstring,
  */
 int getopt_long_only(int argc, char * const argv[], const char *optstring,
                      const struct option *longopts, int *longindex) {
-  int i;
-
-
   printf("# getopt_long_only() optstring = %s\n", optstring);
 
-  for (i = 0; optstring[i]; i++) {
-    if (optstring[i + 1] == ':') {
-      printf("# -%c <flag>\n", optstring[i]);
-      printf("-%c @@\n", optstring[i]);
-      i++;
-    } else {
-      printf("# -%c takes no arguments\n", optstring[i]);
-    }
- 
-    printf("# End of -%c section\n\n", optstring[i]);
-  }
-
-  for (i = 0; longopts[i].name; i++) {
-    printf("# Start of --%s section\n", longopts[i].name);
-
-    if (longopts[i].has_arg) {
-      printf("# --%s <flag>\n", longopts[i].name);
-      printf("--%s @@\n", longopts[i].name);
-    } else {
-      printf("# --%s takes no arguments\n", longopts[i].name);
-    }
-
-    printf("# End of --%s flag section\n\n", longopts[i].name);
-  }
+  output_optstring(optstring);
+  output_longopts(longopts);
 
   return -1;
 }
